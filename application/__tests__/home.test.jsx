865cace7c872ec2c2dcf983303746a28581eb838
Christiaan Versfeld <ST10091991@vcconnect.edu.za>
2024-10-06 21:04:17 +0200

    testing half passed

--- application/__tests__/home.test.jsx
+++ application/__tests__/home.test.jsx
@@ -1,11 +1,13 @@
+import React from 'react';
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import Home from "../src/app/page";  // Adjust the import if your Home component is in a different path
+import Home from "../src/app/page"; // Adjust the import path if needed
+console.log(Home);
+
 
 describe("Home Page", () => {
   it("check for relevant text", () => {
     const { getByText } = render(<Home />);
-
     expect(getByText("Get started by editing")).toBeInTheDocument();
     expect(
       getByText("Find in-depth information about Next.js features and API.")
