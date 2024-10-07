eca65a6cfea8e3d2c6319b4f64a163beadef822f
Christiaan Versfeld <ST10091991@vcconnect.edu.za>
2024-10-07 11:26:38 +0200

    Testing : Internal tests passed

--- application/__tests__/home.test.jsx
+++ application/__tests__/home.test.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import Home from "../src/app/page"; // Adjust the import path if needed
+import Home from "../src/app/page.tsx"; // Adjust the import path if needed
 console.log(Home);
 
 
 describe("Home Page", () => {
   it("check for relevant text", () => {
     const { getByText } = render(<Home />);
-    expect(getByText("Get started by editing")).toBeInTheDocument();
-    expect(
-      getByText("Find in-depth information about Next.js features and API.")
-    ).toBeInTheDocument();
+    expect(getByText("This is the home page")).toBeInTheDocument();
   });
 });
